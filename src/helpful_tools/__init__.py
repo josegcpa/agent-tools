c7906ae08d84691c921eb8f6fc5ab874f935a973
josegcpa <jose.almeida@research.fchampalimaud.org>
2026-01-12 12:13:13 +0000

    added download_and_read_pdf to list

--- src/helpful_tools/__init__.py
+++ src/helpful_tools/__init__.py
@@ -3,11 +3,11 @@ from .tools import *
 __all__ = [
     "read_file",
     "write_file",
-    "delete_file",
     "list_files",
     "search_files",
     "interpret_image",
     "read_table",
+    "download_and_read_pdf",
     "load_pdf",
     "web_search",
     "retrieve_webpage",
